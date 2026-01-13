81e53ca65ed7ab4865b2a8f8fd2f88c7139921de
oumaima <elghalbouniomaima@gmail.com>
2026-01-13 12:23:03 +0100

    edit generate_daily_files.py

--- scripts/test_generated_orders.py
+++ scripts/test_generated_orders.py
@@ -2,14 +2,16 @@ import pandas as pd
 import os
 
 DATA_ROOT = "/app/data"  # or your DATA_ROOT
-RUN_DATE = "2026-01-06"  # example date
+RUN_DATE = "2026-01-13"  # example date
 
 # Paths
-local_orders = os.path.join(DATA_ROOT, "tmp", RUN_DATE, "orders.parquet")
-local_stock  = os.path.join(DATA_ROOT, "tmp", RUN_DATE, "stock.parquet")
+local_orders = os.path.join(DATA_ROOT, "raw", RUN_DATE, "orders_market_MKT-001.parquet")
+local_stock  = os.path.join(DATA_ROOT, "raw", RUN_DATE, "stock.parquet")
 
 # Read
 df_orders = pd.read_parquet(local_orders)
+df_stock = pd.read_parquet(local_stock)
 
 # Show data
 print(df_orders.head())
+print(df_stock.head())
